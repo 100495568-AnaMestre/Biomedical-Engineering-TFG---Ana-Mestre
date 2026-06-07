e2a9a34625a3b1fd242d725ff4ec4ed0a6f290bd
100495568-AnaMestre <100495568@alumnos.uc3m.es>
2026-06-07 17:15:51 +0200

    Delete SA-acetone crystallization codes directory

--- SA-acetone crystallization codes/ajusteElipse.m
+++
@@ -1,9 +0,0 @@
-% this is the function to extract the parameters needed to build the
-% elliptical mask
-function params = ajusteElipse(x, y)
-    A = [x.^2, x.*y, y.^2, x, y, ones(length(x),1)];
-    [U, S, V] = svd(A, 'econ');
-    params = V(:,end); 
-    norm_factor = params(1) + params(3);
-    params = params / norm_factor;
-end
